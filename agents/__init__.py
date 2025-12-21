8a444a2169b158129f773eed6a1fa5dd1db33b1e
wangyue <wangyue>
2025-12-21 21:39:23 +0800

    重构完代码了，开始优化提示词

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,9 +3,6 @@
 from agents.workflow import create_multi_agent_workflow, run_multi_agent_workflow
 
 __all__ = [
-    "create_react_agent",
-    "run_react_agent",
-    "ReActAgent",
     "create_multi_agent_workflow",
     "run_multi_agent_workflow",
 ]
--- tools/__init__.py
+++ tools/__init__.py
@@ -3,7 +3,6 @@
 from tools.base import BaseTool
 from tools.file_tools import ReadFileTool
 from tools.repo_tools import FetchRepoMapTool
-from tools.search_tools import SearchRepoTool
 
-__all__ = ["BaseTool", "ReadFileTool", "FetchRepoMapTool", "SearchRepoTool"]
+__all__ = ["BaseTool", "ReadFileTool", "FetchRepoMapTool"]
 
